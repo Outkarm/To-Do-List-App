49a73bc9dbcfec773b34b0a916542152ee8cbe00
Outkarm <108669691+Outkarm@users.noreply.github.com>
2022-12-07 19:15:50 +0000

    add a base to do list app structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
+import './style.css';
 
-import "./style.css";
+const toDoList = [
+  {
+    description: 'work with saba',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'work at the office',
+    completed: false,
+    index: 1,
+  },
+];
 
+const container = document.querySelector('#sub-container');
 
+for (let x = 0; x < toDoList.length; x += 1) {
+  const task = document.createElement('div');
+  task.classList.add('task');
+  container.append(task);
+  const check = document.createElement('input');
+  check.type = 'checkbox';
+  task.append(check);
+  const theTask = document.createElement('p');
+  theTask.innerText = toDoList[x].description;
+  theTask.classList.add('text');
+  task.append(theTask);
+  const remove = document.createElement('i');
+  remove.classList.add('removeBtn');
+  remove.id = 'moreicon';
+  remove.classList.add('fa-solid');
+  remove.classList.add('fa-ellipsis-vertical');
+  task.append(remove);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
+  mode: 'development',
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: "/",
+    publicPath: '/',
   },
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
