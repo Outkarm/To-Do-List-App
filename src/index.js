24d6e4a831329ac829d096ee8ba4f551639ca054
Outkarm <108669691+Outkarm@users.noreply.github.com>
2022-12-14 13:48:49 +0000

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-loop-func */
-import "./style.css";
-import theCheck from "./modules/checkbox.js";
+import './style.css';
+import theCheck from './modules/checkbox.js';
 
 let toDoList = [];
 
@@ -12,117 +12,117 @@ class CreateTask {
   }
 }
 const pushTask = () => {
-  toDoList = JSON.parse(localStorage.getItem("toDoList")) || [];
-  const taskValue = document.querySelector("#entry").value;
+  toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
+  const taskValue = document.querySelector('#entry').value;
   const id = toDoList.length;
   const completed = false;
   const theTask = new CreateTask(taskValue, completed, id);
-  if (taskValue !== "") {
+  if (taskValue !== '') {
     toDoList.push(theTask);
   }
 };
 
 const store = () => {
-  localStorage.setItem("toDoList", JSON.stringify(toDoList));
+  localStorage.setItem('toDoList', JSON.stringify(toDoList));
 };
 
-const container = document.querySelector("#sub-container");
+const container = document.querySelector('#sub-container');
 const createTask = () => {
-  toDoList = JSON.parse(localStorage.getItem("toDoList")) || [];
+  toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
   for (let x = 0; x < toDoList.length; x += 1) {
-    const task = document.createElement("div");
-    const taskC = document.createElement("div");
-    task.classList.add("task");
+    const task = document.createElement('div');
+    const taskC = document.createElement('div');
+    task.classList.add('task');
     taskC.append(task);
     container.append(taskC);
-    const check = document.createElement("input");
-    check.type = "checkbox";
-    check.classList.add("check-box");
+    const check = document.createElement('input');
+    check.type = 'checkbox';
+    check.classList.add('check-box');
     check.id = toDoList[x].id;
-    check.addEventListener("click", () => {
+    check.addEventListener('click', () => {
       theCheck(check);
     });
     task.append(check);
-    const theTask = document.createElement("p");
+    const theTask = document.createElement('p');
     theTask.innerText = toDoList[x].description;
-    theTask.classList.add("text");
+    theTask.classList.add('text');
     if (toDoList[x].completed === true) {
-      theTask.classList.add("cancel");
+      theTask.classList.add('cancel');
       check.checked = true;
-    } else if (theTask.classList.contains("cancel")) {
-      theTask.classList.remove("cancel");
+    } else if (theTask.classList.contains('cancel')) {
+      theTask.classList.remove('cancel');
       check.checked = false;
     }
     task.append(theTask);
-    const taskOptBtn = document.createElement("button");
-    taskOptBtn.classList.add("task-opt-btn");
-    const opt = document.createElement("i");
-    opt.classList.add("removeBtn");
-    opt.id = "moreicon";
-    opt.classList.add("fa-solid");
-    opt.classList.add("fa-ellipsis-vertical");
+    const taskOptBtn = document.createElement('button');
+    taskOptBtn.classList.add('task-opt-btn');
+    const opt = document.createElement('i');
+    opt.classList.add('removeBtn');
+    opt.id = 'moreicon';
+    opt.classList.add('fa-solid');
+    opt.classList.add('fa-ellipsis-vertical');
     taskOptBtn.append(opt);
     task.append(taskOptBtn);
     opt.id = toDoList[x].id;
-    const editTask = document.createElement("i");
-    editTask.classList.add("fa-solid");
-    editTask.classList.add("fa-pen-to-square");
-    editTask.classList.add("vanish");
+    const editTask = document.createElement('i');
+    editTask.classList.add('fa-solid');
+    editTask.classList.add('fa-pen-to-square');
+    editTask.classList.add('vanish');
     task.append(editTask);
-    const remove = document.createElement("i");
-    remove.classList.add("fa-solid");
-    remove.classList.add("fa-trash-can");
-    remove.classList.add("vanish");
+    const remove = document.createElement('i');
+    remove.classList.add('fa-solid');
+    remove.classList.add('fa-trash-can');
+    remove.classList.add('vanish');
     task.append(remove);
-    taskOptBtn.addEventListener("click", () => {
-      opt.classList.add("vanish");
-      editTask.classList.remove("vanish");
-      remove.classList.remove("vanish");
-      editTask.addEventListener("click", () => {
-        const editContainer = document.createElement("div");
-        editContainer.classList.add("edit-sec");
-        const editAreaCbtn = document.createElement("button");
-        editAreaCbtn.classList.add("checkbtn");
-        const eCCancel = document.createElement("i");
-        eCCancel.classList.add("fa-solid");
-        eCCancel.classList.add("fa-xmark");
+    taskOptBtn.addEventListener('click', () => {
+      opt.classList.add('vanish');
+      editTask.classList.remove('vanish');
+      remove.classList.remove('vanish');
+      editTask.addEventListener('click', () => {
+        const editContainer = document.createElement('div');
+        editContainer.classList.add('edit-sec');
+        const editAreaCbtn = document.createElement('button');
+        editAreaCbtn.classList.add('checkbtn');
+        const eCCancel = document.createElement('i');
+        eCCancel.classList.add('fa-solid');
+        eCCancel.classList.add('fa-xmark');
         editAreaCbtn.append(eCCancel);
         editContainer.append(editAreaCbtn);
 
-        editAreaCbtn.addEventListener("click", () => {
+        editAreaCbtn.addEventListener('click', () => {
           if (taskC.contains(editContainer) === true) {
             editContainer.remove();
           }
-          opt.classList.remove("vanish");
-          editTask.classList.add("vanish");
-          remove.classList.add("vanish");
+          opt.classList.remove('vanish');
+          editTask.classList.add('vanish');
+          remove.classList.add('vanish');
           window.location.reload();
         });
 
-        const editArea = document.createElement("input");
+        const editArea = document.createElement('input');
         editArea.value = toDoList[x].description;
-        editArea.classList.add("type-sec");
+        editArea.classList.add('type-sec');
         editContainer.append(editArea);
-        const editAreabtn = document.createElement("button");
-        editAreabtn.classList.add("checkbtn");
+        const editAreabtn = document.createElement('button');
+        editAreabtn.classList.add('checkbtn');
         editContainer.append(editAreabtn);
-        const editEnter = document.createElement("i");
-        editEnter.classList.add("fa-sharp");
-        editEnter.classList.add("fa-solid");
-        editEnter.classList.add("fa-circle-check");
+        const editEnter = document.createElement('i');
+        editEnter.classList.add('fa-sharp');
+        editEnter.classList.add('fa-solid');
+        editEnter.classList.add('fa-circle-check');
         editAreabtn.append(editEnter);
 
-        editAreabtn.addEventListener("click", () => {
+        editAreabtn.addEventListener('click', () => {
           theTask.innerText = editArea.value;
-          toDoList = JSON.parse(localStorage.getItem("toDoList")) || [];
+          toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
           toDoList[x].description = editArea.value;
 
           if (taskC.contains(editContainer) === true) {
             editContainer.remove();
           }
-          opt.classList.remove("vanish");
-          editTask.classList.add("vanish");
-          remove.classList.add("vanish");
+          opt.classList.remove('vanish');
+          editTask.classList.add('vanish');
+          remove.classList.add('vanish');
           store();
           window.location.reload();
         });
@@ -142,8 +142,8 @@ const createTask = () => {
         }
       };
 
-      remove.addEventListener("click", () => {
-        toDoList = JSON.parse(localStorage.getItem("toDoList")) || [];
+      remove.addEventListener('click', () => {
+        toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
         removeTask();
         store();
 
@@ -153,17 +153,17 @@ const createTask = () => {
   }
 };
 
-const enterTask = document.querySelector(".enter");
-enterTask.addEventListener("click", () => {
+const enterTask = document.querySelector('.enter');
+enterTask.addEventListener('click', () => {
   pushTask();
   store();
-  toDoList = JSON.parse(localStorage.getItem("toDoList")) || [];
+  toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
   window.location.reload();
 });
 
-const clearAll = document.querySelector(".clear-all");
-clearAll.addEventListener("click", () => {
-  toDoList = JSON.parse(localStorage.getItem("toDoList")) || [];
+const clearAll = document.querySelector('.clear-all');
+clearAll.addEventListener('click', () => {
+  toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
   toDoList = toDoList.filter((e) => e.completed === false);
   let newId = 0;
   if (toDoList.length > 0) {
@@ -173,7 +173,7 @@ clearAll.addEventListener("click", () => {
     });
   }
   store();
-  toDoList = JSON.parse(localStorage.getItem("toDoList")) || [];
+  toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
   window.location.reload();
 });
 
