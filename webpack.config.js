ff65bd679adb708e62e564cc86aac96e9a650ae0
Outkarm <108669691+Outkarm@users.noreply.github.com>
2022-12-17 13:34:41 +0000

    git hub pages

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: "/",
+    publicPath: "/To-Do-List-App/",
   },
   optimization: {
     runtimeChunk: "single",
